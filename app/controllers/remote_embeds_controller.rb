12ae0b93004511cf8d5836fd5e8af1ea24338df7
Tom Chapin <tchapin@gmail.com>
2017-10-22 00:52:27 -0700

    Enhancing remote embeds

--- app/controllers/remote_embeds_controller.rb
+++ app/controllers/remote_embeds_controller.rb
@@ -1,12 +1,28 @@
 class RemoteEmbedsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def fund_status
-    fund_name = params.fetch(:fund_name, nil)
-    @fund_balance = big_decimal_to_currency(SquareCashFund.find_by_name(fund_name).balance)
+  before_action :set_fund_from_params, only: [:fund_status, :previous_donors]
 
+  def fund_status
+    @fund_balance = big_decimal_to_currency(@fund.balance)
+    @recent_transactions= @fund.square_cash_transactions.order(:created_at).limit(6)
     respond_to do |format|
       format.js
     end
   end
+
+  def previous_donors
+    @all_transactions = @fund.square_cash_transactions.order(:created_at)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def set_fund_from_params
+    fund_name = params.fetch(:fund_name, nil)
+    @fund = SquareCashFund.find_by_name(fund_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   post 'twilio/voice' => 'twilio#voice'
-
   post 'twilio/receive_sms' => 'twilio#receive_sms'
 
   post 'slack/balance' => 'slack#balance'
 
   get 'remote_embeds/fund_status' => 'remote_embeds#fund_status'
+  get 'remote_embeds/previous_donors' => 'remote_embeds#previous_donors'
 
 end

42bee8d3422054de5f095bbf147b2c043dd9d6bd
Tom Chapin <tchapin@gmail.com>
2017-10-22 01:00:55 -0700

    fixing sort orders

--- app/controllers/remote_embeds_controller.rb
+++ app/controllers/remote_embeds_controller.rb
@@ -5,14 +5,14 @@ class RemoteEmbedsController < ApplicationController
 
   def fund_status
     @fund_balance = big_decimal_to_currency(@fund.balance)
-    @recent_transactions= @fund.square_cash_transactions.order(:created_at).limit(6)
+    @recent_transactions= @fund.square_cash_transactions.order(created_at: :desc).limit(6)
     respond_to do |format|
       format.js
     end
   end
 
   def previous_donors
-    @all_transactions = @fund.square_cash_transactions.order(:created_at)
+    @all_transactions = @fund.square_cash_transactions.order(created_at: :desc)
     respond_to do |format|
       format.js
     end
--- app/models/square_cash_fund.rb
+++ app/models/square_cash_fund.rb
@@ -2,7 +2,7 @@ class SquareCashFund < ApplicationRecord
   has_many :square_cash_transactions, dependent: :destroy
 
   def balance
-    most_recent_transaction = self.square_cash_transactions.order(:created_at).last
+    most_recent_transaction = self.square_cash_transactions.order(created_at: :desc).first
     most_recent_transaction.balance rescue BigDecimal.new('0')
   end
 

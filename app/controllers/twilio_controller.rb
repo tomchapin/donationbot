b1a6fca89bd059872f298224b34cb7d0d3a9675b
Tom Chapin <tchapin@gmail.com>
2017-10-23 09:38:50 -0700

    Debugging and hardening code a little bit

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -14,25 +14,25 @@ class TwilioController < ApplicationController
   end
 
   def receive_sms
-    notification_message = nil
     sms_message = params['Body']
 
     fund = SquareCashFund.find_by_phone_number(params['To'])
-    if sms_message && fund
+    if fund
 
-      # Handle SMS messages about money being received
+      # Check for matches
       money_received_match = sms_message.match(/Square Cash: (.*) sent you \$(.*) for (.*). You now have \$(.*) available in your Cash app/)
+      money_spent_match = sms_message.match(/Square Cash: You spent \$(.*) at (.*)/)
+
       if money_received_match
+        puts 'Money received!'
         transaction = fund.square_cash_transactions.create(person_name: money_received_match[1],
                                                            amount: BigDecimal.new(money_received_match[2]),
                                                            message: money_received_match[3],
                                                            balance: BigDecimal.new(money_received_match[4]))
         notification_message = "#{big_decimal_to_currency transaction.amount} donation received from #{transaction.person_name} (#{transaction.message})! The current #{fund.name} balance is now #{big_decimal_to_currency transaction.balance}"
-      end
 
-      # Handle SMS messages about money being spent
-      money_spent_match = sms_message.match(/Square Cash: You spent \$(.*) at (.*)/)
-      if money_spent_match
+      elsif money_spent_match
+        puts 'Money spent!'
         amount_spent = BigDecimal.new(money_spent_match[1])
         new_balance = fund.balance - amount_spent
         transaction = fund.square_cash_transactions.create(person_name: 'Me',
@@ -40,15 +40,20 @@ class TwilioController < ApplicationController
                                                            message: money_spent_match[2],
                                                            balance: new_balance)
         notification_message = "#{big_decimal_to_currency transaction.amount} spent at #{transaction.message}. The current #{fund.name} balance is now #{big_decimal_to_currency transaction.balance}"
+
+      else
+        raise 'Unable to match SMS message to regex patterns!'
       end
 
       # Post the notification message to the fund's Slack channel
-      if notification_message
-        puts notification_message
-        notifier = Slack::Notifier.new fund.slack_webhook_url
-        notifier.ping notification_message
-      end
+      puts notification_message
+      notifier = Slack::Notifier.new fund.slack_webhook_url
+      notifier.ping notification_message
+
+    else
+      raise "Fund in database not found for specified phone number!"
     end
+
   end
 
 end
